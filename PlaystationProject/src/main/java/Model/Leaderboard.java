78341e03c2dec60f7993e7dcb936b1e1ad7be65f
DavidHuynh04 <131493560+DavidHuynh04@users.noreply.github.com>
2024-04-27 20:48:57 -0700

    Add files via upload

---
+++ PlaystationProject/src/main/java/Model/Leaderboard.java
@@ -0,0 +1,34 @@
+package Model;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
+public class Leaderboard{
+    private Account account;
+    List<Account> inOrder;
+
+    public int compareTo(Account other){
+        if (this.account.getLevel() != other.getLevel()){
+            return Integer.compare(this.account.getLevel(), other.getLevel());
+        }
+        return Integer.compare(this.account.getExp(), other.getExp());
+    }
+
+    public List<Account> leaderboard(HashMap<String, Account> accounts){
+        inOrder = new ArrayList<>(accounts.values());
+        Collections.sort(inOrder);
+        return inOrder;
+    }
+    public int getPosition(Account account){
+        int position = 1;
+        for (int i = 0; i < inOrder.size(); i++){
+            if (!account.equals(inOrder.get(i))){
+                position++;
+            }
+            else{
+                break;
+            }
+        }
+        return position;
+    }
+}
---
+++ PlaystationProject/src/main/java/edu/sjsu/cs/View/HelloController.java
@@ -0,0 +1,14 @@
+package edu.sjsu.cs.View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
