20ee432ebf5a1695bf7c9795f6feb4b5dc93873e
Jake Doxey <jakedoxey@gmail.com>
2024-04-29 14:14:19 -0700

    My first commit. The gui would not run for the life of me so I had to move around a handful of files and fix the structure of the project. I also had to download the JavaFX SDK manually and include the lib.xml file

--- Project/src/main/resources/com/example/project/module-info.java
+++ Project/src/main/resources/com/example/project/module-info.java
@@ -6,7 +6,4 @@ module com.example.project {
     opens com.example.project to javafx.fxml;
     exports com.example.project.Model;
     exports com.example.project.View;
-    opens com.example.project.View to javafx.fxml;
-    exports com.example.project.Controller;
-    opens com.example.project.Controller to javafx.fxml;
 }
