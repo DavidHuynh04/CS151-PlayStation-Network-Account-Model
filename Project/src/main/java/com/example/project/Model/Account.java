9c7812fc3761a9ff49600a018d84167daa2cf010
NamNgHH <namnguyen792003@gmail.com>
2024-04-28 20:40:35 -0700

    Added exp requirement and leveling up just need changes it to achievement based when data get here

--- Project/src/main/java/com/example/project/Model/Account.java
+++ Project/src/main/java/com/example/project/Model/Account.java
@@ -8,8 +8,9 @@ public class Account implements Comparable<Account>{
     private String password;
     private int level;
     private int exp;
+    private int position;
     private HashMap<String, Game> library;
-    public Account(String userName, String password, int level, int exp, HashMap<String, Game> library){
+    public Account(String userName, String password, int level, int exp,  HashMap<String, Game> library){
         this.userName = userName;
         this.password = password;
         this.level = level;
@@ -46,12 +47,46 @@ public class Account implements Comparable<Account>{
     public void setExp(int exp) {
         this.exp = exp;
     }
-
     public HashMap<String, Game> getLibrary() {
         return library;
     }
-
     public void setLibrary(HashMap<String, Game> library) {
         this.library = library;
+    }
+    public void addGame(String gameKey, Game game) {
+        // currently being set to game if change when we added achievements/trophy
+        library.put(gameKey, game);
+        int gameExp = 15;
+        exp += gameExp;
+        while (canLevelUp()) {
+            levelUp();
+        }
+    }
+
+    private boolean canLevelUp() {
+        int expRequired;
+        if (level >= 1 && level <= 100) {
+            expRequired = 60;
+        } else if (level >= 101 && level <= 200) {
+            expRequired = 90;
+        } else {
+            expRequired = Integer.MAX_VALUE;
+        }
+        return exp >= expRequired;
+    }
+
+    private void levelUp() {
+        int expRequired;
+        if (level >= 0 && level <= 100) {
+            expRequired = 60;
+        } else if (level >= 101 && level <= 200) {
+            expRequired = 90;
+        } else {
+            expRequired = Integer.MAX_VALUE;
+        }
+        if (exp >= expRequired) {
+            level++;
+            exp -= expRequired;
+        }
     }
 }
