96aab6492b516a7d0598dfb715f071faacce64d3
NamNgHH <namnguyen792003@gmail.com>
2024-04-29 22:46:16 -0700

    Changes to how xp is tally

--- Project/src/main/java/com/example/project/Model/Game.java
+++ Project/src/main/java/com/example/project/Model/Game.java
@@ -10,4 +10,18 @@ public class Game {
         this.gameTitle = gameTitle;
         this.trophyList = trophyList;
     }
+    public boolean addTropphyToGame(Trophy t){
+        if (!this.trophyList.contains(t)){
+            trophyList.add(t);
+            return true;
+        }
+        return false;
+    }
+    public int getTotalTrophyPoints() {
+        int totalPoints = 0;
+        for (Trophy trophy : trophyList) {
+            totalPoints += trophy.getTrophyPoints();
+        }
+        return totalPoints;
+    }
 }
